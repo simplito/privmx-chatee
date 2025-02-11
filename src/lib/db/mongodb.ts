b06952cb0e884ba3571068ddec6785b6c3edffec
Hubert Klim <hklim@simplito.com>
2025-02-11 10:57:23 +0100

    chore: mongodb connection file cleanup

--- src/lib/db/mongodb.ts
+++ src/lib/db/mongodb.ts
@@ -1,47 +1,14 @@
-import { MONGODB_URI, REPLICA_SET } from '@/shared/utils/env';
-import { Db, MongoClient, MongoClientOptions } from 'mongodb';
+import { MONGODB_URI  } from '@/shared/utils/env';
+import {MongoClient, } from 'mongodb';
 
-// const uri = MONGODB_URI;
-// const options: MongoClientOptions = {
-//     replicaSet: REPLICA_SET
-// };
-//
-// console.log({uri})
-//
-//
-// let client;
-// let clientPromise: Promise<MongoClient>;
+const uri = MONGODB_URI || "mongodb://127.0.0.1:27017/Chatee?replicaSet=rs0"; // Fallback for development
 
-// if (process.env.NODE_ENV === 'development') {
-//     let globalWithMongo = global as typeof globalThis & {
-//         _mongoClientPromise?: Promise<MongoClient>;
-//     };
-//
-//     if (!globalWithMongo._mongoClientPromise) {
-//         client = new MongoClient(uri, options);
-//         globalWithMongo._mongoClientPromise = client.connect();
-//     }
-//     clientPromise = globalWithMongo._mongoClientPromise;
-// } else {
-//     try {
-//         client = new MongoClient(uri, options);
-//         clientPromise = client.connect();
-//     }catch(e){
-//        console.error(`[ERROR] MongoDB connection to: ${MONGODB_URI} failed`);
-//        console.error(e);
-//     }
-//
-// }
-
-const uri = MONGODB_URI || "mongodb://user:password@host:port/database?replicaSet=myReplicaSet"; // Fallback for development
-
-console.log(uri)
-let dbClient:MongoClient; // Store the MongoClient instance
+let dbClient:MongoClient;
 
 async function connectToDatabase() {
     try {
         if (!dbClient) { // Only create a new connection if one doesn't exist
-            dbClient = new MongoClient(uri);
+            dbClient = new MongoClient(uri,{replicaSet:"rs0",});
             await dbClient.connect();
             console.log("Connected to MongoDB");
         } else {
@@ -73,7 +40,5 @@ process.on('SIGINT', async () => {
     await closeDatabaseConnection();
     process.exit(0);
 });
-
-// export default clientPromise;
 
 export { connectToDatabase, closeDatabaseConnection };
